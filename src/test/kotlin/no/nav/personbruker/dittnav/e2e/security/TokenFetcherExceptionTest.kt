2de84bb45835a8526147bb92887fa59c2e7b1245
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-14 16:39:26 +0200

    Rettet noen skrivefeil.
    PB-402. Fiske ut token automatisk

--- src/test/kotlin/no/nav/personbruker/dittnav/e2e/security/TokenFetcherExceptionTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/e2e/security/TokenFetcherExceptionTest.kt
@@ -7,22 +7,22 @@ internal class TokenFetcherExceptionTest {
 
     @Test
     fun `added context should be a part of the toString method`() {
-        val expetedKey1 = "key1"
-        val expetedKey2 = "key2"
-        val expetedValue1 = "value1"
-        val expetedValue2 = "value2"
+        val expectedKey1 = "key1"
+        val expectedKey2 = "key2"
+        val expectedValue1 = "value1"
+        val expectedValue2 = "value2"
 
         val msg = "Simulert feil i en test"
         val exception = TokenFetcherException(
             msg,
             Exception("$msg, cause")
         )
-        exception.addContext(expetedKey1, expetedValue1)
-        exception.addContext(expetedKey2, expetedValue2)
+        exception.addContext(expectedKey1, expectedValue1)
+        exception.addContext(expectedKey2, expectedValue2)
 
         val toStringOfException = exception.toString()
 
-        toStringOfException `should contain all` listOf(expetedKey1, expetedValue1, expetedKey2, expetedValue2)
+        toStringOfException `should contain all` listOf(expectedKey1, expectedValue1, expectedKey2, expectedValue2)
     }
 
 }
