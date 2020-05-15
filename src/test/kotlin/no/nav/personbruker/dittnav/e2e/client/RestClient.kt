b30f768dc8bad6faf3ee25c27f6d27e3cf765516
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-15 16:37:29 +0200

    Verifiserer nå at sikkerheten er satt opp for service-en producer.
    PB-401. Verifiser at hele sløyfen fungerer

--- src/test/kotlin/no/nav/personbruker/dittnav/e2e/client/RestClient.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/e2e/client/RestClient.kt
@@ -2,14 +2,19 @@ package no.nav.personbruker.dittnav.e2e.client
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.header
+import io.ktor.client.request.post
 import io.ktor.client.request.request
 import io.ktor.client.request.url
+import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
+import io.ktor.http.contentType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import no.nav.personbruker.dittnav.e2e.ProduceBrukernotifikasjonDto
 import no.nav.personbruker.dittnav.e2e.config.DittNavDockerComposeCommonContext
 import no.nav.personbruker.dittnav.e2e.config.ServiceConfiguration
+import no.nav.personbruker.dittnav.e2e.operations.ServiceOperation
 import no.nav.personbruker.dittnav.e2e.security.TokenInfo
 import org.slf4j.LoggerFactory
 import java.net.URL
@@ -18,9 +23,8 @@ class RestClient(val httpClient: HttpClient) {
 
     val log = LoggerFactory.getLogger(RestClient::class.java)
 
-    suspend fun get(service: ServiceConfiguration, pathToHit: String): String {
-        val baseUrl = DittNavDockerComposeCommonContext.instance.getBaseUrl(service)
-        val completeUrlToHit = URL("$baseUrl$pathToHit")
+    suspend inline fun <reified T> get(service: ServiceConfiguration, pathToHit: String): T = withContext(Dispatchers.IO) {
+        val completeUrlToHit = constructPathToHit(service, pathToHit)
         return@withContext try {
             httpClient.get<T>(completeUrlToHit)
 
@@ -32,8 +36,7 @@ class RestClient(val httpClient: HttpClient) {
     }
 
     suspend inline fun <reified T> getWithToken(service: ServiceConfiguration, pathToHit: String, token: TokenInfo): T = withContext(Dispatchers.IO) {
-        val baseUrl = DittNavDockerComposeCommonContext.instance.getBaseUrl(service)
-        val completeUrlToHit = URL("$baseUrl$pathToHit")
+        val completeUrlToHit = constructPathToHit(service, pathToHit)
         return@withContext try {
             httpClient.request<T> {
                 url(completeUrlToHit)
@@ -46,6 +49,46 @@ class RestClient(val httpClient: HttpClient) {
             log.error(msg)
             throw e
         }
+    }
+
+    suspend inline fun <reified T> post(service: ServiceConfiguration, operation: ServiceOperation, data: ProduceBrukernotifikasjonDto): T {
+        val completeUrlToHit = constructPathToHit(service, operation.path)
+        return try {
+            httpClient.post<T>() {
+                url(completeUrlToHit)
+                method = HttpMethod.Post
+                contentType(ContentType.Application.Json)
+                body = data
+            }
+
+        } catch (e: Exception) {
+            val msg = "Uventet feil skjedde mot $service, klate ikke å gjenomføre et kallet mot $completeUrlToHit"
+            log.error(msg)
+            throw e
+        }
+    }
+
+    suspend inline fun <reified T> postWithToken(service: ServiceConfiguration, operation: ServiceOperation, data: ProduceBrukernotifikasjonDto, tokenInfo : TokenInfo): T {
+        val completeUrlToHit = constructPathToHit(service, operation.path)
+        return try {
+            httpClient.post<T>() {
+                url(completeUrlToHit)
+                method = HttpMethod.Post
+                header(HttpHeaders.Authorization, "Bearer ${tokenInfo.id_token}")
+                contentType(ContentType.Application.Json)
+                body = data
+            }
+
+        } catch (e: Exception) {
+            val msg = "Uventet feil skjedde mot $service, klate ikke å gjenomføre et kallet mot $completeUrlToHit"
+            log.error(msg)
+            throw e
+        }
+    }
+
+    fun constructPathToHit(service: ServiceConfiguration, pathToHit: String): URL {
+        val baseUrl = DittNavDockerComposeCommonContext.instance.getBaseUrl(service)
+        return URL("$baseUrl$pathToHit")
     }
 
 }
