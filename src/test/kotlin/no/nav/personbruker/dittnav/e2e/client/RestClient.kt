2cd9cdbbb42abe9b5db6e257632bd7f59125b850
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-15 15:33:51 +0200

    Lagt til egen get-metode for å kalle endepunkter som er sikret med OIDC.
    PB-401. Verifiser at hele sløyfen fungerer

--- src/test/kotlin/no/nav/personbruker/dittnav/e2e/client/RestClient.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/e2e/client/RestClient.kt
@@ -1,20 +1,45 @@
 package no.nav.personbruker.dittnav.e2e.client
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.header
+import io.ktor.client.request.request
+import io.ktor.client.request.url
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import no.nav.personbruker.dittnav.e2e.config.DittNavDockerComposeCommonContext
 import no.nav.personbruker.dittnav.e2e.config.ServiceConfiguration
+import no.nav.personbruker.dittnav.e2e.security.TokenInfo
 import org.slf4j.LoggerFactory
 import java.net.URL
 
-class RestClient(private val httpClient: HttpClient) {
+class RestClient(val httpClient: HttpClient) {
 
-    private val log = LoggerFactory.getLogger(RestClient::class.java)
+    val log = LoggerFactory.getLogger(RestClient::class.java)
 
     suspend fun get(service: ServiceConfiguration, pathToHit: String): String {
         val baseUrl = DittNavDockerComposeCommonContext.instance.getBaseUrl(service)
         val completeUrlToHit = URL("$baseUrl$pathToHit")
-        return try {
-            httpClient.get(completeUrlToHit)
+        return@withContext try {
+            httpClient.get<T>(completeUrlToHit)
+
+        } catch (e: Exception) {
+            val msg = "Uventet feil skjedde mot $service, klate ikke å gjenomføre et kallet mot $completeUrlToHit"
+            log.error(msg)
+            throw e
+        }
+    }
+
+    suspend inline fun <reified T> getWithToken(service: ServiceConfiguration, pathToHit: String, token: TokenInfo): T = withContext(Dispatchers.IO) {
+        val baseUrl = DittNavDockerComposeCommonContext.instance.getBaseUrl(service)
+        val completeUrlToHit = URL("$baseUrl$pathToHit")
+        return@withContext try {
+            httpClient.request<T> {
+                url(completeUrlToHit)
+                method = HttpMethod.Get
+                header(HttpHeaders.Authorization, "Bearer ${token.id_token}")
+            }
 
         } catch (e: Exception) {
             val msg = "Uventet feil skjedde mot $service, klate ikke å gjenomføre et kallet mot $completeUrlToHit"
