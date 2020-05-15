08e1f655867f277c7969a75ef4391ebd819170fa
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-15 15:26:04 +0200

    Bytter til å bruke den felles konfigurerte HTTP-klienten.
    PB-401. Verifiser at hele sløyfen fungerer

--- src/test/kotlin/no/nav/personbruker/dittnav/e2e/config/UsesTheCommonDockerComposeContext.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/e2e/config/UsesTheCommonDockerComposeContext.kt
@@ -1,8 +1,7 @@
 package no.nav.personbruker.dittnav.e2e.config
 
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
 import no.nav.personbruker.dittnav.e2e.client.RestClient
+import no.nav.personbruker.dittnav.e2e.client.buildHttpClient
 
 /**
  * Hjelpeklasse som tester kan arve fra for å få tilgang til den felles docker-compose-konteksten.
@@ -12,6 +11,6 @@ import no.nav.personbruker.dittnav.e2e.client.RestClient
 open class UsesTheCommonDockerComposeContext {
 
     val dockerComposeContext = DittNavDockerComposeCommonContext.instance
-    val client = RestClient(HttpClient(Apache))
+    val client = RestClient(buildHttpClient())
 
 }
