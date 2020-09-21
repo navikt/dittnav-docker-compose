ad1ff32fdb3fca507b932bd605a3ff8c5315c23b
Robert Larsen <robert.larsen@nav.no>
2020-09-21 09:59:09 +0200

    Legger til valgfritt felt for ekstern varsling i DTO

--- src/test/kotlin/no/nav/personbruker/dittnav/e2e/client/ProduceBrukernotifikasjonDto.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/e2e/client/ProduceBrukernotifikasjonDto.kt
@@ -1,5 +1,7 @@
 package no.nav.personbruker.dittnav.e2e.client
 
-import no.nav.personbruker.dittnav.e2e.client.ProduceDto
-
-data class ProduceBrukernotifikasjonDto(val tekst: String, val link: String = "http://dummylenke.no"): ProduceDto()
+data class ProduceBrukernotifikasjonDto(
+        val tekst: String,
+        val link: String = "http://dummylenke.no",
+        val eksternVarsling: Boolean = false
+) : ProduceDto()
