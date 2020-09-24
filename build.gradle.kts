93b578e13a51417a0be7ff549d25b287f1dd8aa2
Robert Larsen <robert.larsen@nav.no>
2020-09-24 08:11:16 +0200

    Gradle 6 og dittnav-dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 val junitVersion = "5.6.2"
-val kluentVersion = "1.59"
 val testContainersVersion = "1.14.1"
 val ktorVersion = "1.3.1"
 val logbackVersion = "1.2.3"
 val javaJwtVersion = "3.10.3"
 val jacksonVersion = "2.10.4"
+
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -21,31 +21,31 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation(Logback.classic)
 
-    implementation("io.ktor:ktor-client-apache:$ktorVersion") {
-        exclude("org.slf4j", "slf4j-api")
-    }
-    implementation("io.ktor:ktor-client-logging:$ktorVersion")
-    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-json:$ktorVersion")
-    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+    implementation(Ktor.clientApache)
+    implementation(Ktor.clientLogging)
+    implementation(Ktor.clientLoggingJvm)
+    implementation(Ktor.clientJson)
+    implementation(Ktor.clientJackson)
 
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    implementation(Jackson.dataTypeJsr310)
+
+
+
 
     implementation("com.auth0:java-jwt:$javaJwtVersion")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation(Junit.api)
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testRuntimeOnly(Junit.engine)
 
     testImplementation("org.testcontainers:testcontainers:$testContainersVersion") {
         exclude("junit", "junit")
     }
     testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
 
-    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation(Kluent.kluent)
 }
 
 tasks {
