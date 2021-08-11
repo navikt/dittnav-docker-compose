243db748122b1fbcf72f8df2e1635308e73a5ae0
Robert Larsen <robert.larsen@nav.no>
2021-08-11 09:17:00 +0200

    Endrer navn på variabel for databasenavn.

--- db-datapopulator/entrypoint.sh
+++ db-datapopulator/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-DB_USER="${DB_NAME}-user"
-psql postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST/$DB_NAME -f ./testdata.sql;
+DB_USER="${DB_NAME_BRUKERNOTIFIKASJON_CACHE}-user"
+psql postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST/$DB_NAME_BRUKERNOTIFIKASJON_CACHE -f ./testdata.sql;
