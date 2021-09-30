126e113514a85116c332389cad7c7732394bf597
Robert Larsen <robert.larsen@nav.no>
2021-09-30 13:48:32 +0200

    Legger til e2e-tester for ekstern varsling for Innboks-eventer.

--- src/test/kotlin/no/nav/personbruker/dittnav/e2e/innboks/ProduceInnboksDTO.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/e2e/innboks/ProduceInnboksDTO.kt
@@ -5,5 +5,7 @@ import no.nav.personbruker.dittnav.e2e.client.BrukernotifikasjonDTO
 data class ProduceInnboksDTO(
         val tekst: String,
         val link: String = "http://dummylenke.no",
-        val grupperingsid: String = "123"
+        val grupperingsid: String = "123",
+        val eksternVarsling: Boolean = false,
+        val prefererteKanaler: List<String> = emptyList()
 ) : BrukernotifikasjonDTO()
