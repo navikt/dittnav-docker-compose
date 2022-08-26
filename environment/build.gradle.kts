8d925d93381f820a884e8ff88c814ea48f0d99d1
Christopher Santana <christopher.santana@nav.no>
2022-08-26 13:15:21 +0200

    Fixy fishy.

--- environment/build.gradle.kts
+++ environment/build.gradle.kts
@@ -1,16 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    `kotlin-dsl`
     `maven-publish`
     `java-library`
+    kotlin("jvm")
+    kotlin("plugin.serialization")
 }
 
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
-}
+group = "no.nav.personbruker.dittnav"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+dependencies {
+    api(kotlin("stdlib-jdk8"))
+    api("dev.gradleplugins:gradle-api:6.8.3")
+}
+
 
 publishing {
     repositories{
