2608b8cd6cd923d7630c6dc527a60b5f648a5bb5
Christopher Santana <christopher.santana@nav.no>
2022-08-30 10:34:34 +0200

    Fjerner generiske setEnv funksjoner.

--- src/main/kotlin/no/nav/tms/docker/compose/environment/environmentSetup.kt
+++ src/main/kotlin/no/nav/tms/docker/compose/environment/environmentSetup.kt
@@ -6,30 +6,7 @@ fun JavaExec.setupEnvironment(dockerComposeAppConfig: DockerComposeAppConfig) {
     println("Setting predefined environment variables for ${dockerComposeAppConfig.getAppName()}...")
 
     dockerComposeAppConfig.getEnvironment().forEach { (name, value) ->
-        println("$name=$value")
-        environment(name, value)
-    }
-}
-
-fun JavaExec.setEnvVars(envMap: Map<String, String>) {
-    envMap.forEach { (name, value) ->
-        println("$name=$value")
-        environment(name, value)
-    }
-}
-
-fun JavaExec.setEnvVars(vararg envMappings: Map<String, String>) {
-    envMappings.forEach { envMap ->
-        envMap.forEach { (name, value) ->
-            println("$name=$value")
-            environment(name, value)
-        }
-    }
-}
-
-fun JavaExec.setEnvVars(vararg envMappings: Pair<String, String>) {
-    envMappings.forEach { (name, value) ->
-        println("$name=$value")
+        println(" - $name=$value")
         environment(name, value)
     }
 }
